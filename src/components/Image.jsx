5b471abfd938ea93935822bb314ed72d38903198
Muhammad Ashraf <muhammadashraf2921323@gmail.com>
2023-09-09 11:01:30 +0500

    restraunt

---
+++ src/components/Image.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import img from '../img/Pink-background-online-store-page-login-illustration-vector.jpg'
+
+export default function Image() {
+  return (
+        <img src={img} alt=" "/>
+  )
+}
